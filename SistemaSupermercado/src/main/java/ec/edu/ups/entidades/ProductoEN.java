fa2bba2ac922cb09718ccf6c800a561911c63fd0
Usuario <Usuario@DESKTOP-1B1MQ0A>
2019-06-03 12:35:59 -0500

    Creacion de la Clase ProductoEN

---
+++ SistemaSupermercado/src/main/java/ec/edu/ups/entidades/ProductoEN.java
@@ -0,0 +1,57 @@
+package ec.edu.ups.entidades;
+
+public class ProductoEN {
+	
+	private String producto;
+	private String nombre;
+	private String imagen;
+	private double precioCompra;
+	private double precioVenta;
+	private int stock;
+	private boolean estado;
+	public String getProducto() {
+		return producto;
+	}
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public double getPrecioCompra() {
+		return precioCompra;
+	}
+	public void setPrecioCompra(double precioCompra) {
+		this.precioCompra = precioCompra;
+	}
+	public double getPrecioVenta() {
+		return precioVenta;
+	}
+	public void setPrecioVenta(double precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+	
+
+}
