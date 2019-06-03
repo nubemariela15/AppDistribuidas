801a9d524f1d3b6be97f4e8483db0196b7b83366
Usuario <Usuario@DESKTOP-1B1MQ0A>
2019-06-03 14:29:42 -0500

    Creacion de la clase DetalleFacturaEN

---
+++ SistemaSupermercado/src/main/java/ec/edu/ups/entidades/DetalleFacturaEN.java
@@ -0,0 +1,43 @@
+package ec.edu.ups.entidades;
+
+public class DetalleFacturaEN {
+	
+	private int codigo;
+	private String descripcion;
+	private int cantidad;
+	private double precioUnitario;
+	private double precioTotal;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
+	
+
+}
