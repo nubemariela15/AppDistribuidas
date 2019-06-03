b0ee1a655e607528e85962556b4cc9ed8cc4b7ff
NUBEM <NUBEM@172.16.1.252>
2019-06-03 18:04:59 -0500

    Agregacion de clases

---
+++ SistemaSupermercado/src/main/java/ec/edu/ups/entidades/ClienteEN.java
@@ -0,0 +1,78 @@
+package ec.edu.ups.entidades;
+
+import java.util.Date;
+
+public class ClienteEN {
+ private String codigo;
+ private String cedula;
+ private String nombre;
+ private String apellido;
+ private String sexo;
+ private String direccion;
+ private Date fechaNacimiento;
+ private Date fechaRegistro;
+ private String email;
+ private String contrasenia;
+public String getCodigo() {
+	return codigo;
+}
+public void setCodigo(String codigo) {
+	this.codigo = codigo;
+}
+public String getCedula() {
+	return cedula;
+}
+public void setCedula(String cedula) {
+	this.cedula = cedula;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public String getSexo() {
+	return sexo;
+}
+public void setSexo(String sexo) {
+	this.sexo = sexo;
+}
+public String getDireccion() {
+	return direccion;
+}
+public void setDireccion(String direccion) {
+	this.direccion = direccion;
+}
+public Date getFechaNacimiento() {
+	return fechaNacimiento;
+}
+public void setFechaNacimiento(Date fechaNacimiento) {
+	this.fechaNacimiento = fechaNacimiento;
+}
+public Date getFechaRegistro() {
+	return fechaRegistro;
+}
+public void setFechaRegistro(Date fechaRegistro) {
+	this.fechaRegistro = fechaRegistro;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getContrasenia() {
+	return contrasenia;
+}
+public void setContrasenia(String contrasenia) {
+	this.contrasenia = contrasenia;
+}
+ 
+ 
+}
