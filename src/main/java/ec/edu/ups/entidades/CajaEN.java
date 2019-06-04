07dd8917beed5218e8083df06eb0151165a6feda
Nube Mejia <nube.mariela15@gmail.com>
2019-06-03 19:56:07 -0500

    Caja

---
+++ src/main/java/ec/edu/ups/entidades/CajaEN.java
@@ -0,0 +1,22 @@
+package ec.edu.ups.entidades;
+
+public class CajaEN {
+	private String codigo;
+	private String descripcion;
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	 public CajaEN() {
+		 
+	 }
+	
+}
